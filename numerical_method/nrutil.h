cf72f58b99cf253269e82ea591c2857f065c21eb
daigangfan <daigf.15@sem.tsinghua.edu.cn>
2019-04-22 14:14:13 +0800

    finish NRVec

--- numerical_method/nrutil.h
+++ numerical_method/nrutil.h
@@ -59,4 +59,65 @@ public:
 	inline T& operator[](const int i);
 	inline const T&operator[](const int i) const;
 	inline int size() const;
+	~NRVec();
 };
+template<class T>
+NRVec<T>::NRVec() :nn(0), v(0) {};
+template<class T>
+NRVec<T>::NRVec(int n) :nn(n), v(new T[n]) {};
+template<class T>
+NRVec<T>::NRVec(const T&a, int n) :nn(n), v(new T[n]) {
+	for (int i = 0; i < n; i++) {
+		v[i] = a;
+	}
+}
+template<class T>NRVec<T>::NRVec(const T*a, int n) : nn(n), v(new T[n]) {
+	for (int i = 0; i < n; i++)
+	{
+		v[i] = *a++;
+	}
+}
+template<class T>NRVec<T>::NRVec(const NRVec<T>&rhs) :nn(rhs.nn), v(new T[nn]) {
+	for (int i = 0; i < n; i++) {
+		v[i] = rhs[i];
+	}
+}
+template<class T>
+NRVec<T> & NRVec<T>::operator=(const NRVec<T> & rhs)
+{
+	// TODO: insert return statement here
+	if (this != rhs) {
+		if (nn != rhs.nn) {
+			if (v != 0) delete[](v);
+			nn = rhs.nn;
+			v = new T[nn];
+		}
+		for (int i = 0; i < nn; i++) {
+			v[i] = rhs[i];
+		}
+	}
+	return *this;
+}
+template<class T>NRVec<T>& NRVec<T>::operator=(const T& rhs) {
+	for (int i = 0; i < nn; i++) {
+		v[i] = rhs;
+
+	}
+	return *this;
+}
+template<class T>inline T& NRVec<T>::operator[](const int i) {
+	return v[i];
+}
+template<class T>inline const T& NRVec<T>::operator[](const int i)const {
+	return v[i];
+}
+
+template<class T>inline int& NRVec<T>::size()const {
+	return nn;
+}
+
+template<class T>
+NRVec<T>::~NRVec() {
+	if (v != 0) delete[](v);
+
+}
